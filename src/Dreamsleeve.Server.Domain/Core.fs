eacb73b70737d80caf5b55e5e58e75b71d64cb22
Newrite <newrite.work@yandex.ru>
2026-09-26 04:20:48 +0700

    Add core module for server domain logic and time functions

---
+++ src/Dreamsleeve.Server.Domain/Core.fs
@@ -0,0 +1,12 @@
+﻿namespace Dreamsleeve.Server.Domain
+
+open System
+
+[<RequireQualifiedAccess>]
+module Core =
+
+    let toUnixMilliseconds (time: DateTimeOffset) : int64 =
+        time.ToUnixTimeMilliseconds()
+    
+    let fromUnixMilliseconds (value: int64) : DateTimeOffset =
+        DateTimeOffset.FromUnixTimeMilliseconds(value)
