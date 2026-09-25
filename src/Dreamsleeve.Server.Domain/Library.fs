b0cc182a060ee09b2badf76733f7c68c148464fa
Newrite <newrite.work@yandex.ru>
2026-09-25 23:03:09 +0700

    Start write domain

--- src/Dreamsleeve.Server.Domain/Library.fs
+++ src/Dreamsleeve.Server.Domain/Library.fs
@@ -1,5 +1,36 @@
 ﻿namespace Dreamsleeve.Server.Domain
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+
+    
+type PlayerId = uint64
+
+type PlayerName = string
+
+type Player = {
+    PlayerId: PlayerId
+    PlayerName: PlayerName
+}
+
+type ChatMessageId = uint64
+
+type ChatMessageText = string
+
+type ChatMessage = {
+    MessageId: ChatMessageId
+    MessageText: ChatMessageText
+}
+    
+type Chat = {
+    Players: seq<Player>
+    Messages: seq<ChatMessage>
+}
+
+type ChatChannelId = uint64
+
+type ChatChannelName = string
+
+type ChatChannel = {
+    ChannelId: ChatChannelId
+    ChannelName: ChatChannelName
+    ChannelChat: Chat
+}
